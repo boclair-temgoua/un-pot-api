2564ce79116d82a18df3a51fac2c91ada49b079d
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-04-29 21:35:11 +0200

    fix(module): fix cookie

--- src/app/utils/cookies/cookie-settings.ts
+++ src/app/utils/cookies/cookie-settings.ts
@@ -2,31 +2,33 @@ import { config } from '../../config/index';
 
 export type Env = 'local' | 'prod' | 'test';
 export const cookieSettings = (env: Env) =>
-  env in settingsMap ? settingsMap[(env as Env) ?? 'local'] : settingsMap.local;
+    env in settingsMap
+        ? settingsMap[(env as Env) ?? 'local']
+        : settingsMap.local;
 
 const settingsMap: {
-  [Key in Env]: {
-    httpOnly: boolean;
-    secure: boolean;
-    domain?: string;
-    sameSite: 'none' | 'lax';
-  };
+    [Key in Env]: {
+        httpOnly: boolean;
+        secure: boolean;
+        domain?: string;
+        sameSite: 'none' | 'lax' | 'strict';
+    };
 } = {
-  local: {
-    httpOnly: false,
-    secure: true,
-    sameSite: 'none',
-  },
-  prod: {
-    httpOnly: false,
-    secure: true,
-    domain: config.cookie_access.domain,
-    sameSite: 'none',
-  },
-  test: {
-    httpOnly: false,
-    secure: true,
-    domain: config.cookie_access.domain,
-    sameSite: 'none',
-  },
+    local: {
+        httpOnly: false,
+        secure: true,
+        sameSite: 'none',
+    },
+    prod: {
+        httpOnly: false,
+        secure: true,
+        domain: config.cookie_access.domain,
+        sameSite: 'lax',
+    },
+    test: {
+        httpOnly: false,
+        secure: true,
+        domain: config.cookie_access.domain,
+        sameSite: 'lax',
+    },
 };
